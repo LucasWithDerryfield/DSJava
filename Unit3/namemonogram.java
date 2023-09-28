8f1103eafd8c3aaf74df4d3f4dc8a662043980e8
LucasWithDerryfield <146104070+LucasWithDerryfield@users.noreply.github.com>
2023-09-28 10:25:51 -0400

    first test of monogram

--- Unit3/namemonogram.java
+++ Unit3/namemonogram.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;  // Import the Scanner class
 
+public class Phone {
+    public static void main(String[] args) throws Exception {
+        String fullName = "";
+        String firstName = "";
+        String middleName = "";
+        String lastName = "";
+        char first = "";
+        char second = "";
+        char third = "";
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter your name in the following format: (John Middle Doe):");
+        fullName = scan.nextString();
+        system.out.println(fullName);
+    }
+}
